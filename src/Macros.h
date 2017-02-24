7c60227e3307feb2648fe354836e01f1db7c523b
Kamil Pabin <kamil.pabin@gmail.com>
2017-02-24 18:35:45 +0100

    Add source

---
+++ src/Macros.h
@@ -0,0 +1,16 @@
+#define LEFT_BUTTON   6   //digital6
+#define RIGHT_BUTTON  8   //digital8
+#define UP_BUTTON     7   //digital7
+#define DOWN_BUTTON   A3  //Analog3 jako pin cyfrowy
+#define A_BUTTON      A4  //Analog4 jako pin cyfrowy
+#define B_BUTTON      A5  //Analog5 jako pin cyfrowy
+
+#define SCE           A0
+#define RST           9
+#define DC            10
+#define DN            11
+#define SCLK          13
+#define LED_DISPLAY   12
+
+#define SCREEN_WIDTH 84
+#define SCREEN_HEIGHT 48
